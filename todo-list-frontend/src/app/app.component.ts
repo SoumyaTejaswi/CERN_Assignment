1150dc110edf8d40fc2f2a1f275f7833a380159c
Soumya V <soumya@careeremail.net>
2025-11-08 17:48:52 -0500

    Implemented Search Functionality-Exercise 2

--- todo-list-frontend/src/app/app.component.ts
+++ todo-list-frontend/src/app/app.component.ts
@@ -2,8 +2,8 @@
 
 import {Component} from '@angular/core';
 import {Todo, TodoService} from "./todo.service";
-import {Observable} from "rxjs";
-import { finalize } from 'rxjs/operators';
+import {Observable,BehaviorSubject,combineLatest} from "rxjs";
+import { finalize,map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +15,7 @@ import { finalize } from 'rxjs/operators';
     </div>
     <div class="list">
       <label for="search">Search...</label>
-      <input id="search" type="text">
+      <input id="search" type="text" (input)="onSearchChange($event)">
       <app-progress-bar *ngIf = "isLoading" [isLoading]="isLoading"></app-progress-bar>
       <app-todo-item *ngFor="let todo of todos$ | async" [item]="todo"></app-todo-item>
     </div>
@@ -26,10 +26,25 @@ export class AppComponent {
 
   readonly todos$: Observable<Todo[]>;
   isLoading = true;
+  searchTerm = new BehaviorSubject<string>('');//Adding a new BehaviorSubject to store the search term.
 
   constructor(todoService: TodoService) {
     this.todos$ = todoService.getAll().pipe(
       finalize(() => this.isLoading = false)
     );
+    this.todos$ = combineLatest([this.todos$, this.searchTerm]).pipe(//filtering logic
+      map(([todos, searchTerm]) => {
+        if(!searchTerm.trim()) {
+          return todos;
+        }
+        return todos.filter(todo => 
+          todo.task.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      })
+    );
+  }
+  onSearchChange(event : Event ): void {//method to handle input search change
+    const value = (event.target as HTMLInputElement).value;
+    this.searchTerm.next(value);
   }
 }
